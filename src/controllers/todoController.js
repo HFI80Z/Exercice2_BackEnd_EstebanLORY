5c54c9af5650265bdb5682bec8bd40305a2a7570
Esteban <loryesteban@icloud.com>
2025-10-16 10:41:25 +0200

    First commit Project initialisation

---
+++ src/controllers/todoController.js
@@ -0,0 +1,21 @@
+import Todo from "../models/todoModel.js";
+
+const todoModel = new Todo();
+
+export default class TodoController {
+  static list(req, res) {
+    res.json(todoModel.getAll());
+  }
+  static add(req, res) {
+    const { title } = req.body;
+    if (!title) return res.status(400).json({ error: "title requis" });
+    const newTask = todoModel.addTask(title);
+    res.status(201).json(newTask);
+  }
+  static delete(req, res) {
+    const id = parseInt(req.params.id);
+    const deleted = todoModel.deleteTask(id);
+    if (!deleted) return res.status(404).json({ error: "introuvable" });
+    res.json({ message: "supprimé" });
+  }
+}
